11a573c66a81ec7371d24589e07a5899f15b73a2
lucas <ls.marinho36@gmail.com>
2025-10-10 05:16:20 -0300

    add comment in ProductService

--- src/main/java/com/ecommerce/services/ProductService.java
+++ src/main/java/com/ecommerce/services/ProductService.java
@@ -10,17 +10,20 @@ import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+
+//Classe de serviço
 @Service
 public class ProductService {
-
+    //Injeção de dependências
     private final ProductRepository productRepository;
     private final ProductMapper productMapper;
-
+    //acesso ao banco de dados
+    //conversão entre DTOs e entidades
     public ProductService(ProductRepository productRepository, ProductMapper productMapper) {
         this.productRepository = productRepository;
         this.productMapper = productMapper;
     }
-
+    //Lista produtos por nome, transação apenas para leitura, + segurança e performance
     @Transactional(readOnly = true)
     public Page<ProductOutputDTO> list(String q, Pageable pageable) {
         Page<Product> page;
@@ -31,21 +34,21 @@ public class ProductService {
         }
         return page.map(productMapper::toDTO);
     }
-
+    //cria um novo produto, rollback em caso de erro
     @Transactional
     public ProductOutputDTO create(ProductInputDTO dto) {
         Product product = productMapper.toEntity(dto);
         Product saved = productRepository.save(product);
         return productMapper.toDTO(saved);
     }
-
+    //busca produto por id, retorna exceção caso não encontrada
     @Transactional(readOnly = true)
     public ProductOutputDTO getById(Long id) {
         Product p = productRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Product not found with id " + id));
         return productMapper.toDTO(p);
     }
-
+    //atualiza um produto existente
     @Transactional
     public ProductOutputDTO update(Long id, ProductInputDTO dto) {
         Product p = productRepository.findById(id)
@@ -54,7 +57,7 @@ public class ProductService {
         Product saved = productRepository.save(p);
         return productMapper.toDTO(saved);
     }
-
+    //exclui um produto por ID
     @Transactional
     public void delete(Long id) {
         if (!productRepository.existsById(id)) {
