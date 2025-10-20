297c0deaf86b17079427ccef3c8ebfa4fed813c5
lucas <ls.marinho36@gmail.com>
2025-10-20 12:45:41 -0300

    refresh, modifying pom.xml and include user/role model

--- src/main/java/com/ecommerce/dto/ProductOutputDTO.java
+++
@@ -1,6 +0,0 @@
-package com.ecommerce.dto;
-
-import java.math.BigDecimal;
-
-// DTO de saída
-public record ProductOutputDTO(Long id, String name, String description, BigDecimal price, Integer stock, boolean active) {}
--- src/main/java/com/ecommerce/entities/Role.java
+++
@@ -1,6 +0,0 @@
-package com.ecommerce.entities;
-
-public enum Role {
-    ROLE_ADMIN,
-    ROLE_CLIENT
-}
