3bfe32260686951921d7d36618c3f2827c114c04
lucas <ls.marinho36@gmail.com>
2025-10-20 12:49:43 -0300

    include product model

--- src/main/java/com/ecommerce/model/User.java
+++ src/main/java/com/ecommerce/model/User.java
@@ -1,18 +1,9 @@
 package com.ecommerce.model;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
+
 
 @Entity
 @Table(name = "user")
